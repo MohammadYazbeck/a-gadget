17db1b4ea095256b44f306dbf57017410f9d5265
Mohammad Yazbeck <mohammadyazbeckk@gmail.com>
2024-08-31 02:14:29 +0300

    Fix routing issue

--- src/ui/buttons/FilterOptionButton.jsx
+++ src/ui/buttons/FilterOptionButton.jsx
@@ -9,7 +9,12 @@ export default function FilterOptionButton({ text }) {
     return (
         <button
             className={`border-1 m-2 h-7 px-2 font-semibold text-gray-500 shadow-md hover:border-none hover:bg-gray-100 hover:text-red-600 hover:transition-colors sm:h-10 sm:w-24 sm:text-base ${searchParams.get('filter') === text.toLowerCase() ? 'text-red-400' : 'text-gray-500'} ${isDefaultFilter ? 'text-red-400' : 'text-gray-500'}`}
-            onClick={() => setSearchParams({ filter: text.toLowerCase() })}
+            onClick={() =>
+                setSearchParams(
+                    { filter: text.toLowerCase() },
+                    { replace: true }
+                )
+            }
         >
             {text}
         </button>
